688986fd93a2d3d66ddc2722c17f93166c065817
unknown <yabhijeet7@gmail.com>
2025-07-28 01:50:28 +0530

    first commit for cold email generator

---
+++ app/main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
+
+from chains import Chain
+# from portfolio import Portfolio
+from utils import clean_text
+
+
+def create_streamlit_app(llm, clean_text):
+    st.title("📧 Cold Mail Generator")
+    url_input = st.text_input("Enter a Job URL:", value="https://jobs.nike.com/job/R-33460")
+    url_portfolio = st.text_input("Enter a Portfolio/Resume URL(Optional):", value="")
+    submit_button = st.button("Generate Email")
+
+    if submit_button:
+        try:
+            loader = WebBaseLoader([url_input])
+            data = clean_text(loader.load().pop().page_content)
+            # portfolio.load_portfolio()
+            jobs = llm.extract_jobs(data)
+            for job in jobs:
+                skills = job.get('skills', [])
+                # links = portfolio.query_links(skills)
+                email = llm.write_mail(job, url_portfolio)
+                st.code(email, language='markdown')
+        except Exception as e:
+            st.error(f"An Error Occurred: {e}")
+
+
+if __name__ == "__main__":
+    chain = Chain()
+    # portfolio = Portfolio()
+    st.set_page_config(layout="wide", page_title="Cold Email Generator", page_icon="📧")
+    create_streamlit_app(chain, clean_text)
+
+
